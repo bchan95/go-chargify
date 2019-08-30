c82d6099bd92ad26e166a393dea003d1a3e02870
Brendan Chan <brendan@projetwalrus.com>
2019-08-30 13:52:49 -0300

    Fixed calls not using uri, added product.go for holding calls for retrieving Products.

---
+++ product.go
@@ -0,0 +1,39 @@
+package chargify
+
+type Product struct {
+	ID                      int64             `json:"id"`
+	Name                    string            `json:"name"`
+	Handle                  string            `json:"handle"`
+	Description             string            `json:"description"`
+	AccountingCode          string            `json:"accounting_code"`
+	PriceInCents            int64             `json:"price_in_cents"`
+	Interval                int64             `json:"interval"`
+	IntervalUnit            string            `json:"interval_unit"`
+	InitialChargeInCents    int64             `json:"initial_charge_in_cents"`
+	ExpirationInterval      int64             `json:"expiration_interval"`
+	ExpirationIntervalUnit  string            `json:"expiration_interval_unit"`
+	TrialPriceInCents       int64             `json:"trial_price_in_cents"`
+	TrialInterval           int64             `json:"trial_interval"`
+	TrialIntervalUnit       string            `json:"trial_interval_unit"`
+	InitialChargeAfterTrial bool              `json:"initial_charge_after_trial"`
+	ReturnParams            string            `json:"return_params"`
+	RequestCreditCard       bool              `json:"request_credit_card"`
+	RequireCreditCard       bool              `json:"require_credit_card"`
+	CreatedAt               string            `json:"created_at"`
+	UpdatedAt               string            `json:"updated_at"`
+	ArchivedAt              string            `json:"archived_at"`
+	UpdateReturnURL         string            `json:"update_return_url"`
+	UpdateReturnParams      string            `json:"update_return_params"`
+	ProductFamily           *ProductFamily    `json:"product_family"`
+	PublicSignupPage        *PublicSignupPage `json:"public_signup_page"`
+	Taxable                 bool              `json:"taxable"`
+	VersionNumber           int64             `json:"version_number"`
+}
+
+type ProductFamily struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Handle      string `json:"handle"`
+	AccountCode string `json:"account_code"`
+	Description string `json:"description"`
+}
--- subscription.go
+++ subscription.go
@@ -80,44 +80,6 @@ type SubscriptionCancel struct {
 	ReasonCode          string `json:"reason_code"`
 }
 
-type Product struct {
-	ID                      int64             `json:"id"`
-	Name                    string            `json:"name"`
-	Handle                  string            `json:"handle"`
-	Description             string            `json:"description"`
-	AccountingCode          string            `json:"accounting_code"`
-	PriceInCents            int64             `json:"price_in_cents"`
-	Interval                int64             `json:"interval"`
-	IntervalUnit            string            `json:"interval_unit"`
-	InitialChargeInCents    int64             `json:"initial_charge_in_cents"`
-	ExpirationInterval      int64             `json:"expiration_interval"`
-	ExpirationIntervalUnit  string            `json:"expiration_interval_unit"`
-	TrialPriceInCents       int64             `json:"trial_price_in_cents"`
-	TrialInterval           int64             `json:"trial_interval"`
-	TrialIntervalUnit       string            `json:"trial_interval_unit"`
-	InitialChargeAfterTrial bool              `json:"initial_charge_after_trial"`
-	ReturnParams            string            `json:"return_params"`
-	RequestCreditCard       bool              `json:"request_credit_card"`
-	RequireCreditCard       bool              `json:"require_credit_card"`
-	CreatedAt               string            `json:"created_at"`
-	UpdatedAt               string            `json:"updated_at"`
-	ArchivedAt              string            `json:"archived_at"`
-	UpdateReturnURL         string            `json:"update_return_url"`
-	UpdateReturnParams      string            `json:"update_return_params"`
-	ProductFamily           *ProductFamily    `json:"product_family"`
-	PublicSignupPage        *PublicSignupPage `json:"public_signup_page"`
-	Taxable                 bool              `json:"taxable"`
-	VersionNumber           int64             `json:"version_number"`
-}
-
-type ProductFamily struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Handle      string `json:"handle"`
-	AccountCode string `json:"account_code"`
-	Description string `json:"description"`
-}
-
 type PublicSignupPage struct {
 	ID  int64  `json:"id"`
 	URL string `json:"url"`
@@ -217,7 +179,7 @@ func (req *SubscriptionRequest) Update(client Client, subscriptionID string) (re
 	}
 	var res *http.Response
 	uri := fmt.Sprintf("subscriptions/%s.json", subscriptionID)
-	res, err = client.Put(jsonReq, subscriptionID)
+	res, err = client.Put(jsonReq, uri)
 	if err != nil {
 		return
 	}
@@ -240,7 +202,7 @@ func GetSubscription(client Client, subscriptionID string) (response *Subscripti
 		return nil, errors.New("no id")
 	}
 	var res *http.Response
-	uri := fmt.Sprintf("subscriptions/%s.json", subscriptionID)
+	uri := fmt.Sprintf("subscriptions/%s.json", uri)
 	res, err = client.Get(uri)
 	if err != nil {
 		return
@@ -273,7 +235,7 @@ func (req *SubscriptionRequest) CancelDelayed(client Client) (err error) {
 	}
 
 	uri := fmt.Sprintf("subscriptions/%s/delayed_cancel.json", req.CancelRequest.subscriptionID)
-	_, err = client.Post(jsonReq, req.CancelRequest.subscriptionID)
+	_, err = client.Post(jsonReq, uri)
 	return
 }
 
