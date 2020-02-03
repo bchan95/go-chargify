3817672fb7577d6d2f5654ef0ad01f46842da2f8
Brendan Chan <brendan@projetwalrus.com>
2020-02-03 17:37:49 -0400

    Added create and preview for migrations.

---
+++ migrations.go
@@ -0,0 +1,95 @@
+package chargify
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Migration struct {
+	Id        int64          `json:"-"`
+	Migration *MigrationBody `json:"migration,omitempty"`
+}
+
+type MigrationBody struct {
+	ProductId            int64 `json:"product_id,omitempty"`
+	ProductPricePointId  int64 `json:"product_price_point_id,omitempty"`
+	IncludeTrial         bool  `json:"include_trial,omitempty"`
+	IncludeInitialCharge bool  `json:"include_initial_charge,omitempty"`
+	IncludeCoupons       bool  `json:"include_coupons,omitempty"`
+	PreservePeriod       bool  `json:"preserve_period,omitempty"`
+}
+
+type MigrationResponse struct {
+	Migration *MigrationPreview `json:"migration"`
+}
+
+type MigrationPreview struct {
+	ProratedAdjustmentInCents int64 `json:"prorated_adjustment_in_cents,omitempty"`
+	ChargeInCents             int64 `json:"charge_in_cents,omitempty"`
+	PaymentDueInCents         int64 `json:"payment_due_in_cents,omitempty"`
+	CreditAppliedInCents      int64 `json:"credit_applied_in_cents,omitempty"`
+}
+
+// Migrate a subscription from one product to another.
+// NOTE: This will not update any component price points associated with the product, so update those first.
+func (m *Migration) Create(client Client) (response *Migration, err error) {
+	if m.Migration == nil {
+		return nil, errors.New("missing request")
+	}
+	// have to nest this because chargify is a mess
+	var jsonReq []byte
+	jsonReq, err = json.Marshal(m)
+	if err != nil {
+		return
+	}
+	var res *http.Response
+	res, err = client.Post(jsonReq, fmt.Sprintf("subscriptions/%d/migrations.json", m.Id))
+	if err != nil {
+		return
+	}
+	if err = checkError(res); err != nil {
+		return
+	}
+	defer res.Body.Close()
+	var body []byte
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	response = new(Migration)
+	err = json.Unmarshal(body, response)
+	return
+}
+
+// Preview a migration before creating it.
+func (m *Migration) Preview(client Client) (response *MigrationResponse, err error) {
+	if m.Migration == nil {
+		return nil, errors.New("missing request")
+	}
+	// have to nest this because chargify is a mess
+	var jsonReq []byte
+	jsonReq, err = json.Marshal(m)
+	if err != nil {
+		return
+	}
+	var res *http.Response
+	res, err = client.Post(jsonReq, fmt.Sprintf("subscriptions/%d/migrations/preview.json", m.Id))
+	if err != nil {
+		return
+	}
+	if err = checkError(res); err != nil {
+		return
+	}
+	defer res.Body.Close()
+	var body []byte
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	response = new(MigrationResponse)
+	err = json.Unmarshal(body, response)
+	return
+}
---
+++ migrations_test.go
@@ -0,0 +1,177 @@
+package chargify
+
+import (
+	"backend-services/chargify/test"
+	"bytes"
+	"encoding/json"
+	"github.com/golang/mock/gomock"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestMigration_Create(t *testing.T) {
+	type fields struct {
+		Id        int64
+		Migration *MigrationBody
+	}
+	type args struct {
+		client Client
+	}
+	ctrl := gomock.NewController(t)
+	client := test.NewMockClient(ctrl)
+	tests := []struct {
+		name         string
+		fields       fields
+		args         args
+		stub         func()
+		wantResponse *Migration
+		wantErr      bool
+	}{
+		{
+			name: "create",
+			fields: fields{
+				Id: 1,
+				Migration: &MigrationBody{
+					ProductId:            1,
+					ProductPricePointId:  1,
+					IncludeTrial:         false,
+					IncludeInitialCharge: false,
+					IncludeCoupons:       false,
+					PreservePeriod:       false,
+				},
+			},
+			args: args{
+				client: client,
+			},
+			stub: func() {
+				res, _ := json.Marshal(&Migration{
+					Migration: &MigrationBody{
+						ProductId:            1,
+						ProductPricePointId:  1,
+						IncludeTrial:         false,
+						IncludeInitialCharge: false,
+						IncludeCoupons:       false,
+						PreservePeriod:       false,
+					},
+				})
+				client.EXPECT().Post(gomock.Any(), "subscriptions/1/migrations.json").Return(
+					&http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(bytes.NewBuffer(res)),
+					}, nil)
+			},
+			wantResponse: &Migration{
+				Migration: &MigrationBody{
+					ProductId:            1,
+					ProductPricePointId:  1,
+					IncludeTrial:         false,
+					IncludeInitialCharge: false,
+					IncludeCoupons:       false,
+					PreservePeriod:       false,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.stub != nil {
+				tt.stub()
+			}
+			m := &Migration{
+				Id:        tt.fields.Id,
+				Migration: tt.fields.Migration,
+			}
+			gotResponse, err := m.Create(tt.args.client)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotResponse, tt.wantResponse) {
+				t.Errorf("Create() gotResponse = %v, want %v", gotResponse, tt.wantResponse)
+			}
+		})
+	}
+}
+
+func TestMigration_Preview(t *testing.T) {
+	type fields struct {
+		Id        int64
+		Migration *MigrationBody
+	}
+	type args struct {
+		client Client
+	}
+	ctrl := gomock.NewController(t)
+	client := test.NewMockClient(ctrl)
+	tests := []struct {
+		name         string
+		fields       fields
+		args         args
+		stub         func()
+		wantResponse *MigrationResponse
+		wantErr      bool
+	}{
+		{
+			name: "create",
+			fields: fields{
+				Id: 1,
+				Migration: &MigrationBody{
+					ProductId:            1,
+					ProductPricePointId:  1,
+					IncludeTrial:         false,
+					IncludeInitialCharge: false,
+					IncludeCoupons:       false,
+					PreservePeriod:       false,
+				},
+			},
+			args: args{
+				client: client,
+			},
+			stub: func() {
+				res, _ := json.Marshal(&MigrationResponse{
+					Migration: &MigrationPreview{
+						ProratedAdjustmentInCents: 10,
+						ChargeInCents:             200,
+						PaymentDueInCents:         180,
+						CreditAppliedInCents:      10,
+					},
+				})
+				client.EXPECT().Post(gomock.Any(), "subscriptions/1/migrations/preview.json").Return(
+					&http.Response{
+						StatusCode: 200,
+						Body:       ioutil.NopCloser(bytes.NewBuffer(res)),
+					}, nil)
+			},
+			wantResponse: &MigrationResponse{
+				Migration: &MigrationPreview{
+					ProratedAdjustmentInCents: 10,
+					ChargeInCents:             200,
+					PaymentDueInCents:         180,
+					CreditAppliedInCents:      10,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.stub != nil {
+				tt.stub()
+			}
+			m := &Migration{
+				Id:        tt.fields.Id,
+				Migration: tt.fields.Migration,
+			}
+			gotResponse, err := m.Preview(tt.args.client)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Preview() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotResponse, tt.wantResponse) {
+				t.Errorf("Preview() gotResponse = %v, want %v", gotResponse, tt.wantResponse)
+			}
+		})
+	}
+}
