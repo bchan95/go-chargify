5b7ddfc4b31e6ec74b07dc44b65b6db4db011534
Brendan Chan <brendan@projetwalrus.com>
2020-12-11 11:33:32 -0400

    Add query params.

--- statement.go
+++ statement.go
@@ -6,11 +6,11 @@ import (
 	"io/ioutil"
 )
 
-func GetSubscriptionStatements(client Client, subscriptionId int64, statements *interface{}) error {
+func GetSubscriptionStatements(client Client, subscriptionId int64, pageNumber int32, perPage int32, statements interface{}) error {
 	if subscriptionId == 0 {
 		return NoID()
 	}
-	uri := fmt.Sprintf("subscriptions/%d/statements.json", subscriptionId)
+	uri := fmt.Sprintf("subscriptions/%d/statements.json?direction=desc&per_page=%d&page=%d", subscriptionId, perPage, pageNumber)
 	res, err := client.Get(uri)
 	if err != nil {
 		return err
@@ -26,7 +26,7 @@ func GetSubscriptionStatements(client Client, subscriptionId int64, statements *
 	return json.Unmarshal(body, statements)
 }
 
-func GetStatement(client Client, statementId int64, statement *interface{}) error {
+func GetStatement(client Client, statementId int64, statement interface{}) error {
 	if statementId == 0 {
 		return NoID()
 	}
